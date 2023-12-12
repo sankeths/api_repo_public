9464e89456cad825f9fb96ac61b720e189a0d72a
s.sanketh@gmail.com <s.sanketh@gmail.com>
2023-12-12 09:59:03 +0530

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyOTMyNg/spectral.js?branch_slug=main&token=3d223361-149d-47aa-8ee5-142f4993b3ed";
+export default { extends: ruleset };
